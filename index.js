040f3ad74cb48c4d139c55a6fb2727480d12c69b
TOXIPAIN <109121365+ToxiPain@users.noreply.github.com>
2024-07-07 05:36:04 -0600

    Index 1.0 (ToxiPain)
    "Use strict" - Hace que el código unicamente tome las variables declaradas. 
    
    Luego al pasar el codigo de TS a JS se pasa a usar "import" por "const" y "from" por "require" para que no de errores de tipo ES6
    
    Se usa el MakeWaSocket default de nuestro Node_Modules de modo que no usamos una versión sacada de un archivo como "/SRC" y luego se indican las dependencias que se utilizaran, me aseguré de tomar todas las dependencias necesarias para la conexión, las demas que parecian ser para mensajes y otras cosas las omití. 
    
    Boom se pasa a constante igual y bajo la misma ruta de importanción. 
    
    FS - Salia en la parde de devices de conexión y se usaba en example.ts por lo que se puso transformado a JS 
    
    Const MainLogger lo saco de example.ts, este trae archivos provenientes de Logger.js o simple.js de /SRC por lo que intuyo que se debe crear de cero con las configuraciones que nos interesa para el bot ya que en toda la documentación solo tiene información para programarla pero no para usarla directamente como otras partes de la biblioteca.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Codigo "Start" y configuración de conexión del bot. 
+// Codigo de inicio del Bot (start)
+//-
+// Importar constantes de Baileys
+"use strict";
+const { default: makeWASocket, initInMemoryKeyStore, DisconnectReason, useMultiFileAuthState, makeInMemoryStore, } = require("@whiskeysockets/baileys");
+const { Boom } = require('@hapi/boom');
+const fs = require("fs");
+const { MAIN_LOGGER } = require("./ruta/logger"); //Equivalente a "Logger.js" o "Simple.js"
+//Faltan readers
