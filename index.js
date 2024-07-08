7573416c0f953b5bf11172ac6560c52eca6af6be
TOXIPAIN <109121365+ToxiPain@users.noreply.github.com>
2024-07-08 17:40:14 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,12 @@
 //-
 // Importar constantes de Baileys
 "use strict";
-const { default: makeWASocket, initInMemoryKeyStore, BufferJSON, DisconnectReason, delay, AnyMessageContent, useMultiFileAuthState, makeInMemoryStore, } = require("@whiskeysockets/baileys");
+const { default: makeWASocket, initInMemoryKeyStore, DisconnectReason, useMultiFileAuthState, makeInMemoryStore, } = require("@whiskeysockets/baileys");
 const { Boom } = require('@hapi/boom');
 const fs = require("fs");
+const logg = require('pino');
 const { MAIN_LOGGER } = require("./ruta/logger"); //Equivalente a "Logger.js" o "Simple.js"
-//Faltan readers
+
+const useStore = !process.argv.includes('--no-store'); //Se encarga de procesar MakeInMemory Store
+const usePairingCode = process.argv.includes('--use-pairing-code'); // Se usa para la parte del QE
+const useMobile = process.argv.includes('--mobile'); //Weas de Native Mobile API
