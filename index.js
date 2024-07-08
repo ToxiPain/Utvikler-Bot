404c5decba4170439701354f79ddb1da935f075d
TOXIPAIN <109121365+ToxiPain@users.noreply.github.com>
2024-07-08 17:49:19 -0600

    Update index.js

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ const logg = require('pino');
 const { MAIN_LOGGER } = require("./ruta/logger"); //Equivalente a "Logger.js" o "Simple.js"
 
 const useStore = !process.argv.includes('--no-store'); //Se encarga de procesar MakeInMemory Store
-const usePairingCode = process.argv.includes('--use-pairing-code'); // Se usa para la parte del QE
+const usePairingCode = process.argv.includes('--use-pairing-code'); // Se usa para la parte del QR
 const useMobile = process.argv.includes('--mobile'); //Weas de Native Mobile API
