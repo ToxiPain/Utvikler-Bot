8316a95033110691e5ad0f37ef0458af125d4577
TOXIPAIN <109121365+ToxiPain@users.noreply.github.com>
2024-07-08 18:53:45 -0600

    Update index.js (35-60 example.ts)

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
-// Codigo de inicio del Bot (start)
-//-
-// Importar constantes de Baileys
+// **Importar Dependencias**
 "use strict";
 const { default: makeWASocket, initInMemoryKeyStore, BufferJSON, DisconnectReason, delay, AnyMessageContent, useMultiFileAuthState, makeInMemoryStore, } = require("@whiskeysockets/baileys");
 const { Boom } = require('@hapi/boom');
 const fs = require("fs");
 const logg = require('pino');
-const { MAIN_LOGGER } = require("./ruta/logger"); //Equivalente a "Logger.js" o "Simple.js"
+const NodeCache = require('node-cache');
+const { MAIN_LOGGER } = require("./ruta/logger"); //PENDIENTE POR EDITAR
 
 const useStore = !process.argv.includes('--no-store'); //Se encarga de procesar MakeInMemory Store
 const usePairingCode = process.argv.includes('--use-pairing-code'); // Se usa para la parte del QR
 const useMobile = process.argv.includes('--mobile'); //Weas de Native Mobile API
+
+const msgRetryCounterCache = new NodeCache();
+
+// **Iniciar Conexión**
+async function fanStart() {
+    const connectToWhatsApp = async () => {
+        const { state, saveCreds } = await useMultiFileAuthState('Utvikler_Sesion');
+        const store = makeInMemoryStore({ logger: logg().child({ level: 'fatal', stream: 'store' }) }); 
+        const conn = makeWASocket({ 
+          printQRInTerminal: true,
+            logger: logg({ level: 'fatal' }), auth: state, browser: ["Utvikler Bot", "Safari", "3.0"],
+            getMessage: async key => ({})
+        });
+        title();
+        // 
+        store.bind(conn.ev);
