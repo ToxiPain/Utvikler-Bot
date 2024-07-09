22ffb927115948c8ab2b7e8c097ace82a2fcca02
TOXIPAIN <109121365+ToxiPain@users.noreply.github.com>
2024-07-08 20:30:27 -0600

    Update index.js

--- index.js
+++ index.js
@@ -4,14 +4,33 @@ const { default: makeWASocket, initInMemoryKeyStore, BufferJSON, DisconnectReaso
 const { Boom } = require('@hapi/boom');
 const fs = require("fs");
 const logg = require('pino');
-const NodeCache = require('node-cache');
+const figlet = require("figlet"); // Estilo de texto
+const clui = require('clui'); // Permite meterle modificaciones a la consola
+const { Spinner } = clui; // Complemento de Clui para añadir color
+const moment = require('moment'); // Alternativa a readline para JS
+const { color, mylog, infolog } = require("./lib/color"); // Importar color
 const { MAIN_LOGGER } = require("./ruta/logger"); //PENDIENTE POR EDITAR
-
+// **Constantes**
 const useStore = !process.argv.includes('--no-store'); //Se encarga de procesar MakeInMemory Store
 const usePairingCode = process.argv.includes('--use-pairing-code'); // Se usa para la parte del QR
 const useMobile = process.argv.includes('--mobile'); //Weas de Native Mobile API
+// ¿¿??
 
-const msgRetryCounterCache = new NodeCache();
+// Guardar la sesión en:
+let session = `./${setting.sessionName}.json`;
+
+// **Crear texto de Conexión**
+function title() {
+    console.clear();
+    console.log(figlet.textSync('Utvikler', {
+        font: 'Standard',
+        horizontalLayout: 'default',
+        verticalLayout: 'default',
+        width: 50,
+        whitespaceBreak: false
+    }));
+    console.log(`Whatsapp Bot made by ToxiPain`);
+}
 
 // **Iniciar Conexión**
 async function fanStart() {
