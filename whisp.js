c9d790e3d24e9b078d436a34755e7bbbc3dc4510
Rafa477-tech <rafa477.050@gmail.com>
2024-07-13 05:36:48 -0400

    Update whisp.js

--- whisp.js
+++ whisp.js
@@ -1,3 +1,15 @@
-// Este archivo se encarga de definir el envio de mensajes y otras configuraciones. 
+module.exports = async (conn, msg, m, setting, store) => {
+    try {
+        let { ownerNumber, botName, gamewaktu, limitCount } = setting
+        const { type, quotedMsg, mentioned, now, fromMe } = msg
+        if (msg.isBaileys) return
+        const content = JSON.stringify(msg.message)
+        const from = msg.key.remoted
 
-Hola Mundo :v
+if (prefijo) {
+    var prefix = /^[./#@]/;
+} else {
+    prefix = [];
+} 
+    }
+
